7adff8653d432bc4ddb81bc3073bef52735035c5
Gabriel Paulo <gabrielrodriguespaulo15@gmail.com>
2024-01-06 19:27:00 -0300

    creating docker compose

---
+++ prisma/migrations/20240106220336_new_gyms_check_ins/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Made the column `password_hash` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "gyms" ALTER COLUMN "phone" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "password_hash" SET NOT NULL;
